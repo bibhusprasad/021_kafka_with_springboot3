76936a8f96b8c1e4e250c9eb68bcbcd4155c2f7e
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2024-08-12 22:09:50 +0530

    Kafka consumer json deserializer error handling

--- order-service-using-kafka/src/main/java/com/bibhu/learn/kafka/orderservice/configuration/KafkaOrderServiceConfiguration.java
+++ order-service-using-kafka/src/main/java/com/bibhu/learn/kafka/orderservice/configuration/KafkaOrderServiceConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -30,11 +31,16 @@ public class KafkaOrderServiceConfiguration {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
+
         props.put(JsonDeserializer.TYPE_MAPPINGS, "orderCreated:com.bibhu.learn.kafka.orderservice.message.OrderCreated");
         props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.bibhu.learn.kafka.orderservice.message.OrderCreated");
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.bibhu.learn.kafka.orderservice");
+
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
