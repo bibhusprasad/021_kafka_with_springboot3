22f0320d0c13e1b7fbc5833a0a2dde1ea81d22d0
Bibhu Prasad Samal <bibhusprasad@gmail.com>
2024-08-18 13:54:02 +0530

    Integration test for Order Service

--- order-service-using-kafka/src/test/java/com/bibhu/learn/kafka/orderservice/service/OrderDispatchServiceTest.java
+++ order-service-using-kafka/src/test/java/com/bibhu/learn/kafka/orderservice/service/OrderDispatchServiceTest.java
@@ -15,7 +15,6 @@ import static java.util.UUID.randomUUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
